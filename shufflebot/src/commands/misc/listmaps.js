afb7b7069a3be6f79cc22e77d5be66cb98913ad5
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-02 18:22:45 -0500

    asdffa
    dfsafdfasf

--- shufflebot/src/commands/misc/listmaps.js
+++ shufflebot/src/commands/misc/listmaps.js
@@ -36,7 +36,7 @@ function votesToStars(upvotes,downvotes) {
   function starsToString(stars) {
     let string = '';
     for (let i = 0; i < 5; i++) {
-      if (i-2 < Math.round(stars)) {
+      if (i > Math.round(stars)) {
         string += '★';
       } else {
         string += '☆';
