4b62e82f68248726fde83f8f128f8d7d43aead8d
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-02 18:54:56 -0500

    mmm
    mmmm

--- shufflebot/src/commands/misc/listmaps.js
+++ shufflebot/src/commands/misc/listmaps.js
@@ -14,7 +14,11 @@ module.exports = {
         for (chosenMap of maps) {
             const name = chosenMap.name
             ///const stars = ///starsToString(votesToStars(chosenMap.upvotes, chosenMap.downvotes))
-            const stars = (chosenMap.upvotes - chosenMap.downvotes)/(chosenMap.upvotes+chosenMap.downvotes)
+            if ((chosenMap.upvotes+chosenMap.downvotes) === 0) {
+                var stars = 0
+            } else {
+                const stars = (chosenMap.upvotes - chosenMap.downvotes)/(chosenMap.upvotes+chosenMap.downvotes)
+            }
             const tier = `T${chosenMap.tier}`
             readablemaps.push(`${stars} / ${tier} / ${name}`)
         }
