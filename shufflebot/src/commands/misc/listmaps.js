1140ef0a3f4704dc05cc8b2e1c422dd3d8636efd
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-03 12:00:09 -0500

    Update listmaps.js

--- shufflebot/src/commands/misc/listmaps.js
+++ shufflebot/src/commands/misc/listmaps.js
@@ -54,7 +54,7 @@ module.exports = {
         const collectorFilter = i => i.user.id === interaction.user.id;
 
         try {
-	        const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 6000_000 });
+	        const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 6000000 });
 
           if(confirmation.customId === 'PageLeft') {
             if (pageNumber > 1) {
