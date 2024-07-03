9a3ea3e4b0eb880a8b26f7c322d27b62cefe32cb
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-03 12:59:56 -0500

    Update listmaps.js

--- shufflebot/src/commands/misc/listmaps.js
+++ shufflebot/src/commands/misc/listmaps.js
@@ -28,7 +28,7 @@ module.exports = {
                 }
 
                 const readablemaps = selectedMaps.map(chosenMap => {
-                    const name = chosenMap.name.replace((chosenMap.name).split('surf_')[0], '')
+                    const name = (chosenMap.name.replace((chosenMap.name).split('_')[0], '')).replace('_', '')
                     const stars = votesToStars(chosenMap.upvotes.length, chosenMap.downvotes.length);
                     const tier = `T${chosenMap.tier}`;
                     return `${starsToString(stars)} | ${tier} | ${name}`;
