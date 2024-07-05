f4019bd08343ec70dea835d3fc5f04e69e71d15c
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-05 15:02:40 -0500

    Update listmaps.js

--- shufflebot/src/commands/misc/listmaps.js
+++ shufflebot/src/commands/misc/listmaps.js
@@ -28,12 +28,12 @@ module.exports = {
                 
 
 
-                const selectedMaps = maps.map(async chosenMap => {
+                const selectedMaps = maps.map(chosenMap => {
                     const name = (chosenMap.name)
                     const query = ({userId: interaction.user.id}, {map: name})
                     const stars = votesToStars(chosenMap.upvotes.length, chosenMap.downvotes.length);
                     const tier = `T${chosenMap.tier}`;
-                    var mapRecordForMap = await record.findOne(query)
+                    var mapRecordForMap = record.findOne(query)
                     if (mapRecordForMap) {
                       return `~~${starsToString(stars)} | ${tier} | ${name}~~ ${mapRecordForMap.time}`
                     } else {
@@ -43,111 +43,109 @@ module.exports = {
                 if (selectedMaps.length === 0) {
                   break; // No more maps to display
                 }
+                
+                if (sortingMode === 1) {
+                  console.log(selectedMaps)
+                  selectedMaps.sort((a, b) => {
+                    const nameA = a.split('| surf_')[1]
+                    const nameB = b.split('| surf_')[1]
+                    if (nameA < nameB) {
+                      return -1;
+                    }
+                    if (nameA > nameB) {
+                      return 1;
+                    }
+                  
+                    // names must be equal
+                    return 0;
+                  });
+                } else if (sortingMode === 2) {
+                  selectedMaps.sort()
+                } else if (sortingMode === 3) {
+                  selectedMaps.sort((a, b) => {
+                    const nameA = a.split('| T')[1]
+                    const nameB = b.split('| T')[1]
+                    if (nameA < nameB) {
+                      return -1;
+                    }
+                    if (nameA > nameB) {
+                      return 1;
+                    }
+                  
+                    // names must be equal
+                    return 0;
+                  });
+                }
+                const readablemaps = selectedMaps.slice(startIndex, endIndex);
+                const readableMapsString = (readablemaps).join('\n');
 
-                Promise.all(selectedMaps).then(async resolvedMaps => {
-                  if (sortingMode === 1) {
-                    console.log(resolveddMaps)
-                    resolvedMaps.sort(async (a, b) => {
-                      const nameA = a.split('| surf_')[1]
-                      const nameB = b.split('| surf_')[1]
-                      if (nameA < nameB) {
-                        return -1;
-                      }
-                      if (nameA > nameB) {
-                        return 1;
-                      }
-                    
-                      // names must be equal
-                      return 0;
-                    });
-                  } else if (sortingMode === 2) {
-                    resolvedMaps.sort()
-                  } else if (sortingMode === 3) {
-                    resolvedMaps.sort(async (a, b) => {
-                      const nameA = a.split('| T')[1]
-                      const nameB = b.split('| T')[1]
-                      if (nameA < nameB) {
-                        return -1;
-                      }
-                      if (nameA > nameB) {
-                        return 1;
-                      }
-                    
-                      // names must be equal
-                      return 0;
-                    });
-                  }
-                  const readablemaps = resolvedMaps.slice(startIndex, endIndex);
-                  const readableMapsString = (readablemaps).join('\n');
-  
-                  const embed = new EmbedBuilder()
-                      .setTitle('Map list')
-                      .setDescription(readableMapsString)
-                      .setFooter({text: `Page ${pageNumber}`});
-  
-                  const row = new ActionRowBuilder();
-                  buttons.forEach((role) => {
-                      row.components.push(
-                          new ButtonBuilder()
-                              .setCustomId(role.id)
-                              .setLabel(role.label)
-                              .setStyle(ButtonStyle.Primary)
-                      );
-                  });
-  
-                 const response = await interaction.editReply({
-                      content: '⠀', // Workaround for a Discord API bug where an empty string might cause the embed not to display
-                      components: [row],
-                      embeds: [embed],
-                  });
-  
-                  const collectorFilter = i => i.user.id === interaction.user.id;
-                  const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 600000 });
-  
-                  if (confirmation.customId === 'PageLeft') {
-                      if (pageNumber > 1) {
-                          pageNumber--;
-                          await confirmation.update('⠀')
-                      } else {
+                const embed = new EmbedBuilder()
+                    .setTitle('Map list')
+                    .setDescription(readableMapsString)
+                    .setFooter({text: `Page ${pageNumber}`});
+
+                const row = new ActionRowBuilder();
+                buttons.forEach((role) => {
+                    row.components.push(
+                        new ButtonBuilder()
+                            .setCustomId(role.id)
+                            .setLabel(role.label)
+                            .setStyle(ButtonStyle.Primary)
+                    );
+                });
+
+               const response = await interaction.editReply({
+                    content: '⠀', // Workaround for a Discord API bug where an empty string might cause the embed not to display
+                    components: [row],
+                    embeds: [embed],
+                });
+
+                const collectorFilter = i => i.user.id === interaction.user.id;
+                const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 600000 });
+
+                if (confirmation.customId === 'PageLeft') {
+                    if (pageNumber > 1) {
+                        pageNumber--;
                         await confirmation.update('⠀')
-                      }
-                  } else if (confirmation.customId === 'PageRight') {
-                    if (pageNumber < Math.ceil(maps.length/20)) {
-                      pageNumber++;
-                      await confirmation.update('⠀')
                     } else {
                       await confirmation.update('⠀')
                     }
-                  } else if (confirmation.customId === 'ModeA') {
-                    if (!(sortingMode===1)) {
-                      sortingMode = 1
-                      await confirmation.update('⠀')
-                    } else {
-                      await confirmation.update('⠀')
-                    }
-                  } else if (confirmation.customId === 'ModeB') {
-                    if (!(sortingMode===2)) {
-                      sortingMode = 2
-                      await confirmation.update('⠀')
-                    } else {
-                      await confirmation.update('⠀')
-                    }
-                  } else if (confirmation.customId === 'ModeC') {
-                    if (!(sortingMode===3)) {
-                      sortingMode = 3
-                      await confirmation.update('⠀')
-                    } else {
-                      await confirmation.update('⠀')
-                    }
+                } else if (confirmation.customId === 'PageRight') {
+                  if (pageNumber < Math.ceil(maps.length/20)) {
+                    pageNumber++;
+                    await confirmation.update('⠀')
+                  } else {
+                    await confirmation.update('⠀')
                   }
-                })
+                } else if (confirmation.customId === 'ModeA') {
+                  if (!(sortingMode===1)) {
+                    sortingMode = 1
+                    await confirmation.update('⠀')
+                  } else {
+                    await confirmation.update('⠀')
+                  }
+                } else if (confirmation.customId === 'ModeB') {
+                  if (!(sortingMode===2)) {
+                    sortingMode = 2
+                    await confirmation.update('⠀')
+                  } else {
+                    await confirmation.update('⠀')
+                  }
+                } else if (confirmation.customId === 'ModeC') {
+                  if (!(sortingMode===3)) {
+                    sortingMode = 3
+                    await confirmation.update('⠀')
+                  } else {
+                    await confirmation.update('⠀')
+                  }
+                }
             }
-    } catch(e) {
-      console.log(e)
-      interaction.editReply('timeout')
+        } catch (e) {
+            console.error(e);
+            await interaction.editReply({ content: 'Timeout', components: [] });
+        }
     }
-  }
-}
+};
 
 function votesToStars(upvotes, downvotes) {
     return Math.min(Math.max(Math.round((upvotes) / (downvotes + upvotes) * 5), 0), 5);
