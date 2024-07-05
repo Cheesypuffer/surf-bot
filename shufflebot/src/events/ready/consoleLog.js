ad9159042c183c9add5a77ccffcfb72d49988bf4
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-05 21:37:25 +0000

    j

--- shufflebot/src/events/ready/consoleLog.js
+++ shufflebot/src/events/ready/consoleLog.js
@@ -1,3 +1,6 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online.`)
+    let activities = [`what?`, `surfing probably`, `surf_rainbow 24/7` ],i = 0;   
+    setInterval(() => bot.user.setActivity(`${activities[i++ % activities.length]}`, 
+    {type: PLAYING})
 }
