04a8c870bde845b1e8bf3c0c26036ad7a47560c6
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 15:40:39 -0500

    asdfasfd
    asfsdf

--- shufflebot/src/events/messageCreate/blooTorture.js
+++ shufflebot/src/events/messageCreate/blooTorture.js
@@ -21,7 +21,7 @@ module.exports = async (client, message) => {
                 if (message.content.includes(blooword.toString())) {
                     const banimage = new AttachmentBuilder('https://media.discordapp.net/attachments/1257792531156959303/1259291301489147945/banned.png?ex=668bceaa&is=668a7d2a&hm=693fc251547692b3782f2dc68ed58b32ee929f7ff1391358e2e4f7996f1c9a0e&=&format=webp&quality=lossless')
                     message.channel.send(
-                        {content: 'NO\nhttps://media.discordapp.net/attachments/1257792531156959303/1259291301489147945/banned.png?ex=668bceaa&is=668a7d2a&hm=693fc251547692b3782f2dc68ed58b32ee929f7ff1391358e2e4f7996f1c9a0e&=&format=webp&quality=lossless '},
+                        {content: 'https://media.discordapp.net/attachments/1257792531156959303/1259291301489147945/banned.png?ex=668bceaa&is=668a7d2a&hm=693fc251547692b3782f2dc68ed58b32ee929f7ff1391358e2e4f7996f1c9a0e&=&format=webp&quality=lossless '},
                         {files: [banimage]}
                     )
                     message.delete()
