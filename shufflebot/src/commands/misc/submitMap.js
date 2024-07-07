7b9b58c1c6ea0f33605cc8c2e3e750cd729529d9
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 16:36:56 -0500

    sdafsdf
    asfsdf

--- shufflebot/src/commands/misc/submitMap.js
+++ shufflebot/src/commands/misc/submitMap.js
@@ -51,7 +51,7 @@ module.exports = {
                 name: interaction.options.get('name').value
             }
             const oldMap = await maps.findOne(query)
-            const hasRole = interaction.member.roles.cache.has('1257704302428815521')
+            const hasRole = interaction.member.roles.cache.has(message.guild.roles.cache.find(r => r.name === 'map curator'))
             if (!oldMap && hasRole) {
                 var file = null
                 var map = null
