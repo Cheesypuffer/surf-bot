63e68d02b0350f2c77d7d0bc151efd9f4e9ccf54
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 19:45:24 -0500

    Update getApplicationCommands.js

--- shufflebot/src/utils/getApplicationCommands.js
+++ shufflebot/src/utils/getApplicationCommands.js
@@ -4,8 +4,12 @@ module.exports = async (client, guildId) => {
     if (guildId) {
         const guild = await client.guilds.fetch(guildId)
         applicationCommands = guild.commands
+        console.log('a')
+        console.log(guildId)
     } else {
+        console.log('b')
         applicationCommands = await client.application.commands
+        console.log(applicationCommands)
     }
     
     await applicationCommands.fetch()
