a5995a0f7d9b58aa47e5352cb13890088f9147a2
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 19:32:11 -0500

    Update initializeBot.js

--- shufflebot/src/commands/misc/initializeBot.js
+++ shufflebot/src/commands/misc/initializeBot.js
@@ -16,10 +16,39 @@ module.exports = {
         
         try {
             //The Purifier
-            
+            for (const role in roles) {
+                const oldRole = interaction.guild.roles.cache.find(r => r.name === role)
+                if (!oldRole) {
+                    interaction.guild.roles.create({
+                        data: {
+                            name: role,
+                            color: 'WHITE'
+                        },
+                        reason: 'Initializing the bot'
+                    })
+                }
+
+            }
+
         } catch (error) {
             console.log(error)
             interaction.editReply('Errored')
         }
     }
 }
+
+const roles = [
+    'Tier 0',
+    'Tier 1',
+    'Tier 2',
+    'Tier 3',
+    'Tier 4',
+    'Tier 5',
+    'Tier 6',
+    'Tier 7',
+    'Tier 8',
+    'Tier 9',
+    'Tier 10',
+    'Tier 11',
+    'Tier 12'
+];
