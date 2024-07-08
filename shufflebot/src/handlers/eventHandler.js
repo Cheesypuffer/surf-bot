f83881663e7fc22f914ef01e7b6be1129d6619eb
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 21:41:23 -0500

    Update eventHandler.js

--- shufflebot/src/handlers/eventHandler.js
+++ shufflebot/src/handlers/eventHandler.js
@@ -1,21 +1,39 @@
-const path = require('path')
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const getAllFiles = require("../utils/getAllFiles")
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-module.exports = (client) => {
-    const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true)
-    console.log(eventFolders)
-    for (const eventFolder of eventFolders) {
-        console.log(eventFolder)
-        const eventFiles = getAllFiles(eventFolder);
-        eventFiles.sort((a, b) => {a>b})
-        const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
-        client.on(eventName,async (arg) => {
-            for (const eventFile of eventFiles) {
-                console.log(eventFile)
-                const eventFunction = require(eventFile)
-                await eventFunction(client, arg)
-            }
-        }) 
-    }
+client.commands = new Collection();
+const foldersPath = path.join('/home/runner/surf-bot/shufflebot/src/', 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
