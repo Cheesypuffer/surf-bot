6176d9681e1d1d5dd8504eecd74ff9065bb95bd4
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-07 21:38:28 -0500

    Update eventHandler.js

--- shufflebot/src/handlers/eventHandler.js
+++ shufflebot/src/handlers/eventHandler.js
@@ -4,13 +4,15 @@ const getAllFiles = require("../utils/getAllFiles")
 
 module.exports = (client) => {
     const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true)
-
+    console.log(eventFolders)
     for (const eventFolder of eventFolders) {
+        console.log(eventFolder)
         const eventFiles = getAllFiles(eventFolder);
         eventFiles.sort((a, b) => {a>b})
         const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
         client.on(eventName,async (arg) => {
             for (const eventFile of eventFiles) {
+                console.log(eventFile)
                 const eventFunction = require(eventFile)
                 await eventFunction(client, arg)
             }
