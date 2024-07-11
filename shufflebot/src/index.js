6b1a332a40971a58af139809762eea8292fc0f1d
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-11 17:43:02 -0500

    Update index.js

--- shufflebot/src/index.js
+++ shufflebot/src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const {Client, IntentsBitField, EmbedBuilder, ActivityType, Collection, Events, GatewayIntentBits} = require('discord.js');
 const mongoose = require('mongoose');
-const eventHandler = require('./handlers/eventHandler');
 const fs = require('node:fs');
 const path = require('node:path');
 
