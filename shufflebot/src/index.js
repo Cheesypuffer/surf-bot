0bb12b60787f4f52862b0540974d97728326e0e3
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-11 20:06:17 -0500

    Update index.js

--- shufflebot/src/index.js
+++ shufflebot/src/index.js
@@ -135,32 +135,27 @@ client.on('ready', (c) => {
     }, 2000)
 })
 
-client.on('interactionCreate', async interaction => {
-    console.log('a')
-	if (interaction.isChatInputCommand()) {
-        console.log('b')
-        console.log(interaction.client)
-        console.log(interaction.client.commands)
-        //const command = interaction.client.commands.get(interaction.commandName)
-    
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-    
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-    }
-});
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = interaction.guild.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 
 
