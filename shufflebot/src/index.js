cc0f23e0471efa1e441ee2f14a0289516aec63d3
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-11 19:20:30 -0500

    Update index.js

--- shufflebot/src/index.js
+++ shufflebot/src/index.js
@@ -137,8 +137,12 @@ const rest = new REST().setToken(process.env.TOKEN);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-        console.log(getapplicationcommands(client, guildId))
 		// The put method is used to fully refresh all commands in the guild with the current set
+        const data1 = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: [] },
+		);
+
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
