29390c7bdb622b2769b03c2faa2ef3c61dc59d79
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-12 19:21:26 -0500

    Update index.js

--- shufflebot/src/index.js
+++ shufflebot/src/index.js
@@ -138,15 +138,15 @@ client.on('ready', (c) => {
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.application.commands.get(interaction.commandName)
+	const comand = client.commands.get(interaction.commandName)
 
-	if (!command) {
+	if (!comand) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
 		return;
 	}
 
 	try {
-		await command.execute(interaction);
+		await comand.execute(interaction);
 	} catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
