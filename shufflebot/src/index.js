154c5291088c78c7cfe47fe609056122d2c713a2
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-12 20:29:21 -0500

    Update index.js

--- shufflebot/src/index.js
+++ shufflebot/src/index.js
@@ -101,6 +101,28 @@ for (const folder of commandFolders) {
 	}
 }
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.TOKEN);
 
@@ -135,30 +157,7 @@ client.on('ready', (c) => {
     }, 2000)
 })
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
 
-	const comands = await interaction.client.commands
-	console.log(comands)
-	console.log(comands.type)
-	const comand = await comands.get(interaction.commandName)
-
-	if (!comand) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await comand.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
 
 
 
