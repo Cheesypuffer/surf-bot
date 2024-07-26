062dd92f361a5b1c0c36b0e92ef3e25f0c6ee2cc
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-26 16:51:07 -0500

    AAAd
    aa

--- shufflebot/src/events/interactionCreate.js
+++ shufflebot/src/events/interactionCreate.js
@@ -2,7 +2,32 @@ const {devs, testServer} = require ('../../config.json');
 const getLocalCommands = require('../utils/getApplicationCommands')
 
 module.exports = async (client, interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) {
+        const message = interaction.message
+        console.log(message)
+        console.log(interaction.message.content)
+        console.log(interaction.content)
+        if (interaction.member.roles.cache.some(r => r.name === 'Slimy Weasel')) {
+            //message.react('🥵')
+            var zest = await blootorture.findOne(({}))
+            try {
+                for(const blooword of zest.words) {
+                    if (message.content.includes(blooword.toString())) {
+                        const banimage = new AttachmentBuilder('https://media.discordapp.net/attachments/1257792531156959303/1259291301489147945/banned.png?ex=668bceaa&is=668a7d2a&hm=693fc251547692b3782f2dc68ed58b32ee929f7ff1391358e2e4f7996f1c9a0e&=&format=webp&quality=lossless')
+                        interaction.reply(
+                            {content: gifs[Math.floor(Math.random()*5)]},
+                            {ephemeral : true}
+                        )
+                        interaction.message.delete()
+                        return
+                    }
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        return
+    }
     
     const localCommands = getLocalCommands()
 
