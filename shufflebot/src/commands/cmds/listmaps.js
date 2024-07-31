e444573288782248f162c9a6bd1df72df71a4fd1
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-07-31 14:00:33 -0500

    Update listmaps.js

--- shufflebot/src/commands/cmds/listmaps.js
+++ shufflebot/src/commands/cmds/listmaps.js
@@ -42,6 +42,8 @@ module.exports = {
                     const tier = `T${chosenMap.tier}`;
                     const query3 = ({map: chosenMap.name})
                     for(const dmap in dmapz) {
+                      console.log(dmap.name)
+                      console.log(chosenMap.name)
                       console.log('a')
                       if(dmap.map === chosenMap.name) {
                         var dtier = dmap.dtier
