9be4ab9d9a6d43e34eae1b4aa13ba25cbef00604
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-09-07 10:28:40 -0500

    Delete register-commands.js

--- shufflebot/src/register-commands.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const {Client, IntentsBitField, EmbedBuilder, ActivityType, Collection, Events, GatewayIntentBits, REST, Routes} = require('discord.js');
-const mongoose = require('mongoose');
-const fs = require('node:fs');
-const path = require('node:path');
-
-
