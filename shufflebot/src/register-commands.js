469e8881ad46a9076eb54cd804ed36da5a7da2ef
Cheesypuffer <cheesypufferfish1@gmail.com>
2024-09-18 19:09:51 -0500

    dawdawdwa
    dawdawd

---
+++ shufflebot/src/register-commands.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const {Client, IntentsBitField, EmbedBuilder, ActivityType, Collection, Events, GatewayIntentBits, REST, Routes} = require('discord.js');
+const mongoose = require('mongoose');
+const fs = require('node:fs');
+const path = require('node:path');
+
+
